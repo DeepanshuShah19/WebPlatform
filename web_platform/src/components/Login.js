2c675e14a5f84a4fcc757a07327090a5c844183f
Deepanshu Shah <deepanshushah@deepanshus-air.home>
2023-02-14 16:20:23 -0500

    Registration UI

--- web_platform/src/components/Login.js
+++ web_platform/src/components/Login.js
@@ -1,58 +1,67 @@
 import React, { Component } from "react";
-import { Grid, TextField, Button} from '@material-ui/core';
+import { Grid, TextField, Button } from '@material-ui/core';
 import "../styles/Login.css";
-import {handleLogin} from '../utils/apiCalls';
+import { handleLogin } from '../utils/apiCalls';
 class Login extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      isFormvalid: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            isFormvalid: false,
+            register: false
+        }
     }
-  }
 
-  handleChange = async (event) => {
-    this.setState({
-      [event.target.id]: [event.target.value]
-    }, () => {
-      this.handleFormValidation();
-    })
-  };
+    handleChange = async (event) => {
+        this.setState({
+            [event.target.id]: [event.target.value]
+        }, () => {
+            this.handleFormValidation();
+        })
+    };
 
-  handleFormValidation =async () => {
-    if (this.state.email && this.state.pasword) {
-      this.setState({ isFormvalid: true})
-    } else if(this.state.isFormvalid){
-      this.setState({isFormvalid: false})
+    handleFormValidation = async () => {
+        if (this.state.email && this.state.password) {
+            this.setState({ isFormvalid: true })
+        } else if (this.state.isFormvalid) {
+            this.setState({ isFormvalid: false })
+        }
     }
-  }
-  handleLogin = async () => {
-    let loginResponse = await handleLogin(this.state.email, this.state.password);
-    console.log("loginResponse", loginResponse);
-  }
-  render() {
-    return (
-      <Grid className="app__login">
-        <Grid className="login__container">
-          <Grid className="container_right">
-            <Grid className="form">
-              <Grid>
-                <TextField id="email" required label="Email Id" type="text" onChange={this.handleChange} />
-              </Grid>
-              <Grid>
-                <TextField id="password" required label="Password" type="password" onChange={this.handleChange} />
-              </Grid>
-            </Grid>
-            <Button type="submit" className="login__button" variant="contained" onClick={this.handleLogin} disabled={!this.state.isFormvalid}>
-              Sign In
-            </Button>
-          </Grid>
-        </Grid>
-      </Grid>
-    );
-  }
+    handleLogin = async () => {
+        let loginResponse = await handleLogin(this.state.email, this.state.password);
+        console.log("loginResponse", loginResponse);
+    }
+    render() {
+        return (
+            <>
+                {this.state.register
+                    ? window.location.href = "./register"
+                    : <><Grid className="app__login">
+                        <Grid className="login__container">
+                            <Grid className="container_right">
+                                <Grid className="form">
+                                    <Grid>
+                                        <TextField id="email" required label="Email Id" type="text" onChange={this.handleChange} />
+                                    </Grid>
+                                    <Grid>
+                                        <TextField id="password" required label="Password" type="password" onChange={this.handleChange} />
+                                    </Grid>
+                                </Grid>
+                                <Button className="login__button" variant="contained" onClick={this.handleLogin} disabled={!this.state.isFormvalid}>
+                                    Sign In
+                                </Button>
+
+                                <Button className="login__button" variant="contained" onClick={() => { this.setState({ register: true }) }}>
+                                    New User
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Grid></>}
+            </>
+        );
+    }
 };
 
 export default Login;
--- web_platform/src/components/Register.js
+++ web_platform/src/components/Register.js
@@ -1,161 +1,124 @@
-import React, { useState } from "react";
-import { Button } from "@mui/material";
-import WebhookIcon from "@mui/icons-material/Webhook";
+import React, { Component } from "react";
+import { Grid, TextField, Button } from '@material-ui/core';
+// import WebhookIcon from "@mui/icons-material/Webhook";
 import "../styles/Register.css";
-import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/bootstrap.css";
-import Grid from "@mui/material/Grid";
+import { handleRegistration } from '../utils/apiCalls';
 
-const Register = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password1, setPassword1] = useState("");
-  const [password2, setPassword2] = useState("");
-  const [phone, setPhone] = useState("");
-  const [country, setCountry] = useState("");
-  const [street, setStreet] = useState("");
-  const [province, setProvince] = useState("");
-  const [postalCode, setPostalCode] = useState("");
-
-  const handleRegister = async (e) => {
-    e.preventDefault();
-    console.log(phone);
-
-    if (password1 !== password2) return alert("Password didn't match");
-
-    try {
-      console.log("Sending Request.....");
-      let resp = await fetch("http://localhost:5000/register", {
-        method: "POST",
-        headers: { "content-type": "application/json" },
-        body: JSON.stringify({
-          name: name,
-          password: password1,
-          email: email,
-          phone: phone,
-          country: country,
-          street: street,
-          province: province,
-          postalCode: postalCode,
-        }),
-      });
-      if (resp.status === 200) {
-        alert("User Registered Successfully");
-      } else if (resp.status === 409) {
-        alert("User Already Exists");
-      } else {
-        alert("User Registration Unsuccessfull");
-      }
-    } catch (e) {
-      console.log(e.message);
+class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+            phone: '',
+            country: '',
+            street: '',
+            province: '',
+            postalCode: '',
+            isFormvalid: false
+        }
     }
-  };
 
-  return (
-    <div className="app__register">
-      <div className="register_container">
-        <div className="container_left">
-          <form className="form" onSubmit={handleRegister} action="#">
-            <label for="fullname">Full Name</label>
-            <input
-              id="name"
-              type="text"
-              placeholder="Full Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-            <label for="email">Email</label>
-            <input
-              id="email"
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
+    handleChange = async (event) => {
+        this.setState({
+            [event.target.id]: [event.target.value]
+        }, () => {
+            this.handleFormValidation();
+        })
+    };
 
-            <label for="phone">Phone</label>
-            <PhoneInput
-              className="phoneInput"
-              id="phone"
-              country="CA"
-              value={phone}
-              onChange={(phone) => {
-                setPhone(phone);
-              }}
-              required
-            />
+    handleFormValidation = async () => {
+        if (this.state.email && (this.state.password === this.state.confirmPassword) && this.state.name && this.state.phone
+            && this.state.country && this.state.street && this.state.province && this.state.postalCode) {
+            this.setState({ isFormvalid: true })
+        } else if (this.state.isFormvalid) {
+            this.setState({ isFormvalid: false })
+        }
+    }
 
-            <label for="country">Country</label>
-            <input
-              type="text"
-              id="country"
-              value={country}
-              onChange={(e) => setCountry(e.target.value)}
-              placeholder="Country"
-            />
+    handleRegistration = async () => {
+        let registrationResponse = await handleRegistration(this.state.email, this.state.password);
+        console.log("loginResponse", registrationResponse);
+    }
 
-            <label for="street">Street</label>
-            <br />
-            <input
-              type="text"
-              id="street"
-              value={street}
-              onChange={(e) => setStreet(e.target.value)}
-              placeholder="Street"
-            />
+    // const handleRegister = async (e) => {
+    //   e.preventDefault();
+    //   console.log(phone);
 
-            <label for="postCode">Postal Code</label>
-            <br />
-            <input
-              type="text"
-              id="postalCode"
-              value={postalCode}
-              onChange={(e) => setPostalCode(e.target.value)}
-              placeholder="Postal Code"
-            />
+    //   if (password1 !== password2) return alert("Password didn't match");
 
-            <label for="province">Province</label>
-            <input
-              type="text"
-              id="province"
-              value={province}
-              onChange={(e) => setProvince(e.target.value)}
-              placeholder="Province"
-            />
-
-            <label for="password1">Password</label>
-            <input
-              id="password"
-              minLength={8}
-              type="password"
-              placeholder="Password"
-              value={password1}
-              onChange={(e) => setPassword1(e.target.value)}
-              required
-            />
-            <label for="password2">Confirm Password</label>
-            <input
-              id="password2"
-              type="password"
-              minLength={8}
-              placeholder="Confirm Password"
-              value={password2}
-              onChange={(e) => setPassword2(e.target.value)}
-              required
-            />
-            <Button type="submit" className="login__button" variant="contained">
-              Register
-            </Button>
-          </form>
-        </div>
-        <div className="container_right">
-          <WebhookIcon />
-        </div>
-      </div>
-    </div>
-  );
+    //   try {
+    //     console.log("Sending Request.....");
+    //     let resp = await fetch("http://localhost:5000/register", {
+    //       method: "POST",
+    //       headers: { "content-type": "application/json" },
+    //       body: JSON.stringify({
+    //         name: name,
+    //         password: password1,
+    //         email: email,
+    //         phone: phone,
+    //         country: country,
+    //         street: street,
+    //         province: province,
+    //         postalCode: postalCode,
+    //       }),
+    //     });
+    //     if (resp.status === 200) {
+    //       alert("User Registered Successfully");
+    //     } else if (resp.status === 409) {
+    //       alert("User Already Exists");
+    //     } else {
+    //       alert("User Registration Unsuccessfull");
+    //     }
+    //   } catch (e) {
+    //     console.log(e.message);
+    //   }
+    // };
+    render() {
+        return (
+            <Grid className="app__register">
+                <Grid className="register_container">
+                    <Grid className="container_left">
+                        <Grid className="form">
+                            <Grid>
+                                <TextField id="name" required label="Name" type="text" onChange={this.handleChange} />
+                            </Grid>
+                            <Grid>
+                                <TextField id="email" required label="Email Id" type="text" onChange={this.handleChange} />
+                            </Grid>
+                            <Grid>
+                                <TextField id="phone" required label="Phone Number" type="text" onChange={this.handleChange} />
+                            </Grid>
+                            <Grid>
+                                <TextField id="street" required label="Street name" type="text" onChange={this.handleChange} />
+                            </Grid>
+                            <Grid>
+                                <TextField id="province" required label="Provience" type="text" onChange={this.handleChange} />
+                            </Grid>
+                            <Grid>
+                                <TextField id="postalCode" required label="Postal Code" type="text" onChange={this.handleChange} />
+                            </Grid>
+                            <Grid>
+                                <TextField id="country" required label="Country" type="text" onChange={this.handleChange} />
+                            </Grid>
+                            <Grid>
+                                <TextField id="password" required label="Password" type="password" onChange={this.handleChange} />
+                            </Grid>
+                            <Grid>
+                                <TextField id="confirmPassword" required label="Confirm Password" type="password" onChange={this.handleChange} />
+                            </Grid>
+                        </Grid>
+                        <Button className="login__button" variant="contained" onClick={this.handleRegistration} disabled={!this.state.isFormvalid}>
+                            Sign Up
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        );
+    }
 };
 
 export default Register;
