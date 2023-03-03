d998fafe8eb364ea95710262f11934da574229d7
Deepanshu Shah <deepanshushah@deepanshus-air.home>
2023-03-03 15:13:59 -0500

    Connecting mongodb database

--- server/app.js
+++ server/app.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 app.use(express.json())
+
+const mongoDbURL = "mongodb+srv://deepanshu:university@webportal.iumygie.mongodb.net/?retryWrites=true&w=majority";
+
+//MongoDb Connection
+mongoose
+    .connect(mongoDbURL, {
+        useNewUrlParser: true,
+    }).then(() => {
+        console.log("Connected to database");
+    })
+    .catch((e) => console.log(e));
+
+
+//Defining API Port 12230
 app.listen(12230, () => {
-    console.log("Server started on port 5000");
+    console.log("Server started on port 12230");
 })
 
+//test API
 app.post("/post", async (req, res) => {
     console.log(req.body);
     const { data } = req.body;
