98dd6ae76f9e276a36e0d97483f33ed484b616eb
Deepanshu Shah <deepanshushah@deepanshus-air.home>
2023-03-19 16:29:57 -0400

    save user meeeting

---
+++ server/UserMeeting.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userMeetingSchema = new mongoose.Schema(
+    {
+        EmailId: String,
+        Topic: String,
+        Join_URL: String,
+        Start_URL: String,
+        MeetingId: String,
+        Attendee: [{
+            type: String
+        }]
+    },
+    {
+        collection: "UserMeeting",
+    }
+);
+
+mongoose.model("UserMeeting", userMeetingSchema);
