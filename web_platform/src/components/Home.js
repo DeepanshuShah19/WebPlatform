a74d8c62e8e4389ad9e4bdba78672e96ca4d83c7
sharmaSagar01 <sagaarpkr@gmail.com>
2023-03-20 13:36:18 -0400

    Removed Margin in Home Screen to fix the UI glitch

--- web_platform/src/components/Home.js
+++ web_platform/src/components/Home.js
@@ -31,7 +31,7 @@ class Home extends Component {
                 container
                 component="main"
                 sx={{
-                  marginTop: 8,
+                  // marginTop: 8,
                   height: "100vh",
                   background:
                     "linear-gradient(135deg, #c89abc 0%, #99c7a5 100%)",
